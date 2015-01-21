36fd1af2de6babd1fdf09c0b9ba8a3de5ad158fd
Binh <nguyeb4@uw.edu>
2015-01-20 23:01:18 -0800

    v0.0015 Change cookie and add locking

--- timeserver.go
+++ timeserver.go
@@ -1,13 +1,14 @@
 // Simple program to run a server showing a web page
 // displaying the current time of day
+// Personalize with using cookie for the user name
 // Example usage: http://localhost:8080/time
 // Default port number is 8080
 // Optional flag -V printing version number to console output
 // Optional flag -port for port number
 //
 //	Author: Binh Nguyen
-//	Date: 1/13/2015
-//	Version: 0.0010
+//	Date: 1/20/2015
+//	Version: 0.0015
 
 package main
 
@@ -16,24 +17,30 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"os/exec"
 	"strconv"
+	"strings"
+	"sync"
 	"time"
 )
 
 var (
-	version = "Timeserver version 0.0010"
+	version = "Timeserver version 0.0015"
+	users   = map[string]string{}
+	mutex   = &sync.Mutex{}
 )
 
 // Display the current time
 func timeHandler(response http.ResponseWriter, request *http.Request) {
+	fmt.Println("In time handler")
 	// Check the path
-	if request.URL.Path != "/time" {
-		errorHandler(respone, request)
+	if request.URL.Path != "/time" && request.URL.Path != "/time/" {
+		errorHandler(response, request)
 		return
 	}
 
 	// Current time format
-	const timeFormat = "3:04:052 PM"
+	const timeFormat = "3:04:05 PM"
 
 	// Print the response
 	fmt.Fprintln(response, "<html>")
@@ -44,65 +51,98 @@ func timeHandler(response http.ResponseWriter, request *http.Request) {
 	fmt.Fprintln(response, "</style>")
 	fmt.Fprintln(response, "</head>")
 	fmt.Fprintln(response, "<body>")
-	fmt.Fprintln(response, "The time is now <span class=\"time\">"+
-		time.Now().Format(timeFormat)+"</span>.</p>")
+
+	// Check the cookie from the client
+	cookie, _ := request.Cookie("UserCookie")
+	user := strings.TrimPrefix(cookie.String(), "UserCookie=")
+
+	if users[user] != "" {
+		fmt.Fprintln(response, "The time is now <span class=\"time\">"+
+			time.Now().Format(timeFormat)+"</span>, "+users[user]+".</p>")
+	} else {
+		fmt.Fprintln(response, "The time is now <span class=\"time\">"+
+			time.Now().Format(timeFormat)+"</span>.</p>")
+	}
 	fmt.Fprintln(response, "</body>")
 	fmt.Fprintln(response, "</html>")
 }
 
 // General handler for the home page
 func generalHandler(response http.ResponseWriter, request *http.Request) {
+	fmt.Println("In general handler")
+	fmt.Println(request.URL.Path)
 	// Check the path
 	if request.URL.Path != "/" && request.URL.Path != "/index.html" {
-		errorHandler(respone, request)
+		fmt.Println("Invalid path general handler")
+		errorHandler(response, request)
 		return
 	}
 
 	// Check the cookie from the client
 	cookie, _ := request.Cookie("UserCookie")
+	fmt.Println(cookie.String())
+	user := strings.TrimPrefix(cookie.String(), "UserCookie=")
 
 	// If user is logged in, print the greeting
-	if cookie.Value != "" {
-		fmt.Fprintln(response, "Greetings, "+cookie.Value+".")
-	} else { // Redirect to log in
-		http.Redirect(response, request, "/login?name=name", http.StatusFound)
+	if users[user] != "" {
+		fmt.Fprintln(response, "<html>")
+		fmt.Fprintln(response, "<body>")
+		fmt.Fprintln(response, "Greetings, "+users[user]+".")
+		fmt.Fprintln(response, "</body>")
+		fmt.Fprintln(response, "</html>")
+	} else {
+		// Print the login
+		fmt.Fprintln(response, "<html>")
+		fmt.Fprintln(response, "<body>")
+		fmt.Fprintln(response, "<form action=\"login\">")
+		fmt.Fprintln(response, " What is your name, Earthling?")
+		fmt.Fprintln(response, " <input type=\"text\" name=\"name\" size=\"50\">")
+		fmt.Fprintln(response, " <input type=\"submit\">")
+		fmt.Fprintln(response, "</form>")
+		fmt.Fprintln(response, "</p>")
+		fmt.Fprintln(response, "</body>")
+		fmt.Fprintln(response, "</html>")
 	}
 }
 
 // Login handler for the form
 func loginHandler(response http.ResponseWriter, request *http.Request) {
+	fmt.Println("In login handler")
 	// Check the path
-	if request.URL.Path != "/login?name=name" {
-		errorHandler(respone, request)
+	if request.URL.Path != "/login" {
+		fmt.Println("Invalid path login handler")
+		errorHandler(response, request)
 		return
 	}
 
-	// Print the login
-	fmt.Fprintln(response, "<html>")
-	fmt.Fprintln(response, "<body>")
-	fmt.Fprintln(response, "<form action=\"login\">")
-	fmt.Fprintln(response, " What is your name, Earthling?")
-	fmt.Fprintln(response, " <input type=\"text\" name=\"name\" size=\"50\">")
-	fmt.Fprintln(response, " <input type=\"submit\">")
-	fmt.Fprintln(response, "</form>")
-	fmt.Fprintln(response, "</p>")
-	fmt.Fprintln(response, "</body>")
-	fmt.Fprintln(response, "</html>")
-
 	// Read the name value from the form
-	request.ParseForm()
+	fmt.Println("Parsing form value")
+	// request.ParseForm()
 	userName := request.FormValue("name")
+	bytesUUID, _ := exec.Command("uuidgen").Output()
+	userUUID := fmt.Sprintf("%v", bytesUUID)
+
+	// Synchronization, locking the users for writing
+	fmt.Println("Locking")
+	mutex.Lock()
+	users[userUUID] = userName
+	mutex.Unlock()
+	fmt.Println("Lock released")
 
 	// Set cookies on the client
-	cookie := &http.Cookie{Name: "UserCookie", Value: userName, Expires: time.Now().Add(356 * 24 * time.Hour), HttpOnly: true}
+	cookie := &http.Cookie{Name: "UserCookie", Value: userUUID, Expires: time.Now().Add(356 * 24 * time.Hour), HttpOnly: true}
 	http.SetCookie(response, cookie)
+	fmt.Println("Cookie Set")
+	http.Redirect(response, request, "./..", 302) // redirect to home
+	fmt.Println("Redirected to home")
 }
 
 // Logout handler for the form
 func logoutHandler(response http.ResponseWriter, request *http.Request) {
+	fmt.Println("In logout handler")
 	// Check the path
 	if request.URL.Path != "/logout" {
-		errorHandler(respone, request)
+		errorHandler(response, request)
 		return
 	}
 
@@ -152,10 +192,12 @@ func main() {
 		return
 	}
 
-	http.HandleFunc("/time", timeHandler)             // handle /time path
-	http.HandleFunc("/", generalHandler)              // general path
-	http.HandleFunc("/login?name=name", loginHandler) // login path
-	http.HandleFunc("/logout", logoutHandler)         // logout path
+	http.HandleFunc("/time", timeHandler)          // handle /time path
+	http.HandleFunc("/time/", timeHandler)         // handle /time path
+	http.HandleFunc("/", generalHandler)           // general path
+	http.HandleFunc("/index.html", generalHandler) // general path
+	http.HandleFunc("/login", loginHandler)        // login path
+	http.HandleFunc("/logout", logoutHandler)      // logout path
 	err := http.ListenAndServe(":"+strconv.Itoa(*portPtr), nil)
 
 	// Server error, display message and exit the application
